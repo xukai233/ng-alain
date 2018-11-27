f3d5d95ff219ff3dad5d65aad03882487c5ec51d
ssrivers <ssrivers@me.com>
2018-11-27 16:53:54 +0800

    update nswag settings

--- nswag/generate-service-proxies.ps1
+++ nswag/generate-service-proxies.ps1
@@ -2,23 +2,23 @@
 
 Write-Host 'generate code of service proxies success.'
 
-Get-ChildItem -Include '*.ts' -Recurse | ForEach-Object {
-    (Get-Content -Encoding UTF8 $_) | ForEach-Object {
-        $_ -replace '\/do-create-','/' `
-           -replace '\/do-update-','/' `
-           -replace '\/do-delete-','/' `
-           -replace '\/do-get-','/' `
-           -replace '\/do-create"','"' `
-           -replace 'doCreate','create' `
-           -replace '\/do-update"','"' `
-           -replace 'doUpdate','update' `
-           -replace '\/do-delete"','"' `
-           -replace 'doDelete','delete' `
-           -replace '\/do-get"','"' `
-           -replace 'doGet','get'
-    } | Set-Content $_
-}
-Write-Host 'tags in code already replaced.'
+# Get-ChildItem -Include '*.ts' -Recurse | ForEach-Object {
+#     (Get-Content -Encoding UTF8 $_) | ForEach-Object {
+#         $_ -replace '\/do-create-','/' `
+#            -replace '\/do-update-','/' `
+#            -replace '\/do-delete-','/' `
+#            -replace '\/do-get-','/' `
+#            -replace '\/do-create"','"' `
+#            -replace 'doCreate','create' `
+#            -replace '\/do-update"','"' `
+#            -replace 'doUpdate','update' `
+#            -replace '\/do-delete"','"' `
+#            -replace 'doDelete','delete' `
+#            -replace '\/do-get"','"' `
+#            -replace 'doGet','get'
+#     } | Set-Content $_
+# }
+# Write-Host 'tags in code already replaced.'
 
 Copy-Item 'service-proxies.ts'  -Destination '../src/app/service-proxies/service-proxies.ts'
 
